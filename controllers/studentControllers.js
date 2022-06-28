36eb7b8d9d7eddb8a49b5546b236a88c350e37ae
asrafulhaq <devvorb@gmail.com>
2022-06-29 00:28:58 +0600

    student create, read, show done

--- controllers/studentControllers.js
+++ controllers/studentControllers.js
@@ -1,21 +1,32 @@
+const Student = require("../models/studentModel");
 
 /**
  * @desc get all student data 
  * @name GET /student/
  * @access public 
  */
-const getAllStudents = (req, res) => {
-    res.render('index');
+const getAllStudents = async (req, res) => {
+
+    let students = await Student.find();
+    res.render('index', { students }); 
 }
 
 
+
+
+
 /**
  * @desc get all student data 
  * @name GET /student/
- * @access public 
+ * @access public  
  */
- const getSingleStudent = (req, res) => {
-    res.render('show');
+ const getSingleStudent =  async (req, res) => {
+    let id = req.params.id;
+
+ 
+    let singleData = await Student.findById(id);
+    console.log(singleData);
+    res.render('show', { singleData });  
 }
 
 /**
@@ -34,9 +45,25 @@ const getAllStudents = (req, res) => {
  * @access public 
  */
  const createStudent = (req, res) => {
-    console.log(req);
+
+    // store data to DB
+    Student.create({
+        ...req.body, 
+        photo : req.file.filename
+    });
+
+
+    // redirect to home page 
     res.redirect('/student');
     
+}
+
+const deleteStudent =  async (req, res) => {
+    let id = req.params.id;
+
+    await Student.findByIdAndDelete(id);
+
+    res.redirect('/student');
 }
 
 
@@ -46,5 +73,6 @@ module.exports = {
     getAllStudents, 
     createStudent, 
     getSingleStudent, 
-    showStudentForm
+    showStudentForm, 
+    deleteStudent
 }
--- routes/studentRoute.js
+++ routes/studentRoute.js
@@ -1,11 +1,33 @@
 const express = require('express');
-const { getAllStudents, createStudent, getSingleStudent, showStudentForm } = require('../controllers/studentControllers');
+const { getAllStudents, createStudent, getSingleStudent, showStudentForm, deleteStudent } = require('../controllers/studentControllers');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+ 
+ 
+// multer config 
+const storage = multer.diskStorage({
+    destination : (req, file, cb) => {
+
+        cb(null, path.join(__dirname, '../assets/upload/')); 
+    },
+    filename : (req, file, cb) => {
+        cb(null, Date.now()+'_'+ file.originalname );
+    }
+});
+
+// load multer 
+const studentMulter = multer({
+    storage : storage
+}).single('photo');
+
+
 
 // student route 
+router.get('/delete/:id', deleteStudent);  
 router.get('/create', showStudentForm); 
 router.get('/', getAllStudents); 
-router.post('/', createStudent); 
+router.post('/', studentMulter,  createStudent);  
 router.get('/:id', getSingleStudent); 
  
 
